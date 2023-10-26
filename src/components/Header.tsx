fc74ed6623b6459d82eab0e2c40d816e32d8e159
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-10-26 18:41:59 +0200

    header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import videoBg from '../assets/video/headerVideoBg.mp4'
 
 
 
@@ -6,8 +6,13 @@ export const Header = () => {
 
  
  return(
-  <>
-<p>header</p>
-  </>
+  <div className='header'>
+    <div className="header_overlay"></div>
+    <video src={videoBg} autoPlay loop muted/>
+    <div className="header_content">
+      <h1>Let's Clean</h1>
+      <h2>together <span className='header_content--dot'></span></h2>
+    </div>
+  </div>
  )
 }
