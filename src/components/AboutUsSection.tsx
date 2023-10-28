7d1f8055578e4dde5e8f59523e51362d50caec03
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-10-28 23:02:53 +0200

    about section text and layout

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -1,18 +1,27 @@
+import testImg from '../assets/images/homePage/garbage-path.jpg'
+
 export const AboutUsSection = () => {
  return (
   <>
    <section className="about-us">
     <h1 className="about-us">About Us</h1>
-    <p className="about-us__text">We are a Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis culpa dolore temporibus ea error modi, quo possimus maiores fugit omnis.</p>
+    <p className="about-us__subtitle">We are a Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis culpa dolore temporibus ea error modi, quo possimus maiores fugit omnis.</p>
     <div className="about-us__container">
-     <div>
-       <p className="">Together is a compoany that helps to clean our Planet from garbage.</p>
-       <p>We currently have <span>10</span> offices. </p>
+     <div className='about-us__section about-us__section--first'>
+       <p className="about-us__text">Together is a company that helps to clean our Planet from garbage.</p>
+       <p className="about-us__text">We currently have <span>10</span> offices. </p>
       </div>
-     <div></div>
-     <div>
-      <p className="">There are <span>1209</span> registered volunteers.</p>
-       <p>We haver collected <span>100304kg</span> of waste. </p>
+     <div className='about-us__section about-us__section--middle'>
+      <img src={testImg} alt="" />
+      <div className='about-us__section--img'>
+        <img src={testImg} alt="" />
+        <img src={testImg} alt="" />
+      </div>
+      <img src={testImg} alt="" />
+     </div>
+     <div className='about-us__section about-us__section--last'>
+      <p className="about-us__text">There are <span>1209</span> registered volunteers.</p>
+       <p className="about-us__text">We haver collected <span>100304kg</span> of waste. </p>
      </div>
     </div>
    </section>
